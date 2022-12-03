daefbc3fa8ec8384e7e0a9ab5aece6cab721a11e
Robert Hovey <robert.hovey95@gmail.com>
2022-12-03 11:25:39 -0500

    completed row model with working preview -- needs altering

--- Fitness Tracker/Views/AttributeInputRow+View.swift
+++ Fitness Tracker/Views/AttributeInputRow+View.swift
@@ -1,20 +1,63 @@
+import Combine
 import SwiftUI
 
-struct AttributeInputRow_View: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        // this will be the component used for each row
-        // Need to include:
-            // HStack
-            // Section
-            // Picker or TextField
-        Divider()
-        // each row will always end with a Divider() because the edit and record views begin with a divider
+struct AttributeInputRowView: View {
+    var attributeTitle: String
+    var inputType: inputs
+    // for Picker
+    @Binding var pickerSelection: String
+    var pickerSelections: [String]
+    // for TextField
+    @Binding var textSelection: String
+    var receivingFunction: ((_ value: String) -> String)
+    var trailingText: String?
+    
+    enum inputs {
+        case picker
+        case textfield
     }
+    var body: some View {
+        VStack {
+            HStack {
+                Section(header: Text(attributeTitle).frame(maxWidth: .infinity, alignment: .leading)) {
+                    switch inputType {
+                    case .picker:
+                        Picker(attributeTitle, selection: $pickerSelection) {
+                            ForEach(pickerSelections, id: \.self) { selection in
+                                Text(selection)
+                            }
+                        }
+                    case .textfield:
+                        TextField(attributeTitle, text: $textSelection)
+                            .keyboardType(.numberPad)
+                            .onReceive(Just(textSelection)) { newValue in
+                                let newString = receivingFunction(newValue)
+                                textSelection = newString
+                                }
+                            }
+                        Text(trailingText ?? "")
+                    }
+                }
+                .padding(.horizontal)
+            }
+            Divider()
+    }
+        // each row will always end with a Divider() because the edit and record views begin with a divider
 }
 
-struct AttributeInputRow_View_Previews: PreviewProvider {
+struct AttributeInputRowView_Previews: PreviewProvider {
     static var previews: some View {
-        AttributeInputRow_View()
+        VStack {
+            Divider()
+            AttributeInputRowView(
+                attributeTitle: "Reps",
+                inputType: .picker,
+                pickerSelection: .constant("2"),
+                pickerSelections: ["1", "2", "3"],
+                textSelection: .constant("Text")
+            ) {value in
+                return value
+            }
+        }
     }
 }
