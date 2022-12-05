e2568748ff7b8e798ef9c3a8359301e74528ab55
Robert Hovey <robert.hovey95@gmail.com>
2022-12-05 11:46:26 -0500

    started new attribute components for exercise views

---
+++ Fitness Tracker/Views/AttributeInputRows/AttributeInputPicker+View.swift
@@ -0,0 +1,14 @@
+import Combine
+import SwiftUI
+
+struct AttributeInputPicker_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AttributeInputPicker_View_Previews: PreviewProvider {
+    static var previews: some View {
+        AttributeInputPicker_View()
+    }
+}
---
+++ Fitness Tracker/Views/AttributeInputRows/AttributeInputTextField+View.swift
@@ -0,0 +1,14 @@
+import Combine
+import SwiftUI
+
+struct AttributeInputTextField_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AttributeInputTextField_View_Previews: PreviewProvider {
+    static var previews: some View {
+        AttributeInputTextField_View()
+    }
+}
