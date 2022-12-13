e702ad1d1f79fcbf9d6996550893c598c48d5a90
Robert Hovey <robert.hovey95@gmail.com>
2022-12-12 21:34:49 -0500

    removed useless comment

--- Fitness Tracker/Views/EditExercise/EditExercise+Buttons+View.swift
+++ Fitness Tracker/Views/EditExercise/EditExercise+Buttons+View.swift
@@ -31,7 +31,6 @@ extension EditExerciseView {
                         exercise.intensity = viewModel.intensityLevel
                         exercise.bikeIntensity = Int16(viewModel.bikeIntensityLevel)
                         exercise.length = viewModel.formatTimeStringBackToDouble(minutes: viewModel.minutesElapsedString, seconds: viewModel.secondsElapsedString)
-                        // exercise.dateCompleted = Date.now
                         exercise.reps = viewModel.reps
                         exercise.sets = Int16(viewModel.sets)
                         try? moc.save()
