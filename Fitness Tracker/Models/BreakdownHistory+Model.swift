dc94bbc141d33cdd2d8db0949e70c3b368b5d3ee
Robert Hovey <robert.hovey95@gmail.com>
2022-12-17 08:35:09 -0500

    data is displaying in collapsible row

--- Fitness Tracker/Models/BreakdownHistory+Model.swift
+++ Fitness Tracker/Models/BreakdownHistory+Model.swift
@@ -3,6 +3,17 @@ import SwiftUI
 struct BreakdownHistoryGraph: Identifiable {
     let id = UUID()
     let dateRange: String?
-    let minutesPerDay: Double?
+    let minutesPerDay: [minutesPerDay]?
     let graphs: [BreakdownHistoryGraph]?
+}
+
+struct minutesPerDay: Identifiable {
+    let id = UUID()
+    let dayOfWeek: weekday
+    let time: Double
+    
+}
+
+enum weekday {
+    case mon, tue, wed, thu, fri, sat, sun
 }
--- Fitness Tracker/Views/BreakdownHistory+View.swift
+++ Fitness Tracker/Views/BreakdownHistory+View.swift
@@ -5,11 +5,31 @@ struct BreakdownHistoryView: View {
     @FetchRequest(sortDescriptors: []) var previousWeeks: FetchedResults<PreviousWeek>
     @Environment(\.managedObjectContext) var moc
     @ObservedObject var viewModel = ViewModel()
+
+    // using fetched data to create the same objects
+    func getDateRange() -> [BreakdownHistoryGraph] {
+        var breakdownHistoryGraphs: [BreakdownHistoryGraph] = []
+        for week in previousWeeks {
+            breakdownHistoryGraphs.append(BreakdownHistoryGraph(dateRange: week.weekOf, minutesPerDay: nil, graphs: getTotalTimes()))
+        }
+        return breakdownHistoryGraphs
+    }
     
-    // dummy data to use for testing
-    let graphOne: BreakdownHistoryGraph = .init(dateRange: nil, minutesPerDay: 26.8, graphs: nil)
-    let graphTwo: BreakdownHistoryGraph = .init(dateRange: nil, minutesPerDay: 23.0, graphs: nil)
-    let graphThree: BreakdownHistoryGraph = .init(dateRange: nil, minutesPerDay: 34.2, graphs: nil)
+    func getTotalTimes() -> [BreakdownHistoryGraph] {
+        var graphs: [BreakdownHistoryGraph] = []
+        for week in previousWeeks {
+            graphs.append(BreakdownHistoryGraph(dateRange: nil, minutesPerDay: [
+                minutesPerDay(dayOfWeek: .mon, time: week.minutesMon),
+                minutesPerDay(dayOfWeek: .tue, time: week.minutesTue),
+                minutesPerDay(dayOfWeek: .wed, time: week.minutesWed),
+                minutesPerDay(dayOfWeek: .thu, time: week.minutesThu),
+                minutesPerDay(dayOfWeek: .fri, time: week.minutesFri),
+                minutesPerDay(dayOfWeek: .sat, time: week.minutesSat),
+                minutesPerDay(dayOfWeek: .sun, time: week.minutesSun),
+            ], graphs: nil))
+        }
+        return graphs
+    }
     
     var body: some View {
         let breakdownHistoryGraphs: [BreakdownHistoryGraph] = [
@@ -20,25 +40,21 @@ struct BreakdownHistoryView: View {
         if viewModel.weeksOf.count == 0 {
             Text("Sorry, there is no data to display.")
         } else {
-            List(breakdownHistoryGraphs, children: \.graphs) { week in
-                HStack {
-                    if week.dateRange != nil {
-                        Text(week.dateRange ?? "")
-                    } else {
-                        Text(String(week.minutesPerDay ?? 0.0))
+            List(getDateRange(), children: \.graphs) { week in
+                    HStack {
+                        if week.dateRange != nil {
+                            Text(week.dateRange ?? "")
+                        } else {
+                            Text(String((week.minutesPerDay?[3].time)!))
+                        }
                     }
+                    // should check if any time exists in the week
+                    // if not, do not display week at all
                 }
             }
         }
-        List {
-            ForEach(previousWeeks, id: \.self) { week in
-                Text(week.weekOf ?? "")
-                // should check if any time exists in the week
-                // if not, do not display week at all
-            }
-        }
     }
-}
+
 
 struct BreakdownHistoryView_Previews: PreviewProvider {
     static var previews: some View {
