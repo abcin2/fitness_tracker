027dac7ccd9674d236b2e6817fc22fd360b218e0
Robert Hovey <robert.hovey95@gmail.com>
2022-12-21 21:01:46 -0700

    moved breakdown history view logic to viewmodel

--- Fitness Tracker/ViewModels/BreakdownHistory+ViewModel.swift
+++ Fitness Tracker/ViewModels/BreakdownHistory+ViewModel.swift
@@ -4,46 +4,81 @@ import SwiftUI
 extension BreakdownHistoryView {
     final class ViewModel: ObservableObject {
         
-        func findClosestMondayBeforeToday() -> Date {
-            let currentWeekday = Date.now.formatted(.dateTime.weekday())
-            // only needs to go 1...6
-            if currentWeekday == "Mon" {
-                return Date.now
-            } else {
-                for num in 1...6 {
-                    let selectedWeekday = Calendar.current.date(byAdding: .day, value: -(num), to: Date.now)!.formatted(.dateTime.weekday())
-                    if selectedWeekday == "Mon" {
-                        return Calendar.current.date(byAdding: .day, value: -(num), to: Date.now)!
-                    }
-                }
+        func getDateRange(with data: FetchedResults<Workout>) -> [BreakdownHistoryGraph] {
+            var breakdownHistoryGraphs: [BreakdownHistoryGraph] = []
+            for range in getExercisesForEachRange(with: data).keys {
+                breakdownHistoryGraphs.append(BreakdownHistoryGraph(dateRange: range, minutesPerDay: nil, graphs: getTotalTimes(with: data)))
             }
-            return Date.now
+            return breakdownHistoryGraphs
         }
         
-        func minutesPerDayThisWeek(workouts: FetchedResults<Workout>) -> [String:Double] {
-            var minutesPerDayDict: [String:Double] = [
-                "Mon": 0.0,
-                "Tue": 0.0,
-                "Wed": 0.0,
-                "Thu": 0.0,
-                "Fri": 0.0,
-                "Sat": 0.0,
-                "Sun": 0.0,
-            ]
-            
-            let firstMondayInWeek: Date = findClosestMondayBeforeToday()
-            for nextDay in 0...6 {
-                var minutesExercised: Double = 0.0
-                let currentDay = Calendar.current.date(byAdding: .day, value: nextDay, to: firstMondayInWeek)!.formatted(.dateTime.day().month())
-                for workout in workouts {
-                    let recordedDay = (workout.dateCompleted ?? Date.now).formatted(.dateTime.day().month())
-                    if currentDay == recordedDay {
-                        minutesExercised += workout.length
+        func getExercisesForEachRange(with data: FetchedResults<Workout>) -> [String:[Workout]] {
+            var exercises: [String:[Workout]] = [:]
+            for workout in data {
+                if exercises[workout.dateRange ?? "Uknown Range"] != nil {
+                    exercises[workout.dateRange ?? "Uknown Range"]?.append(workout)
+                } else {
+                    exercises[workout.dateRange ?? "Uknown Range"] = [workout]
+                }
+            }
+            // need to sort ranges
+            return exercises
+        }
+        
+        func getTotalTimes(with data: FetchedResults<Workout>) -> [BreakdownHistoryGraph] {
+            var graphs: [BreakdownHistoryGraph] = []
+            for range in getDateRanges(with: data) {
+                var secondsPerDay: [String:Double] = [
+                    "Mon": 0.0,
+                    "Tue": 0.0,
+                    "Wed": 0.0,
+                    "Thu": 0.0,
+                    "Fri": 0.0,
+                    "Sat": 0.0,
+                    "Sun": 0.0
+                ]
+                for workout in data {
+                    if workout.dateRange == range {
+                        switch workout.dateCompleted?.formatted(.dateTime.weekday()) {
+                        case "Mon":
+                            secondsPerDay["Mon"]! += workout.length
+                        case "Tue":
+                            secondsPerDay["Tue"]! += workout.length
+                        case "Wed":
+                            secondsPerDay["Wed"]! += workout.length
+                        case "Thu":
+                            secondsPerDay["Thu"]! += workout.length
+                        case "Fri":
+                            secondsPerDay["Fri"]! += workout.length
+                        case "Sat":
+                            secondsPerDay["Sat"]! += workout.length
+                        case "Sun":
+                            secondsPerDay["Sun"]! += workout.length
+                        default:
+                            secondsPerDay["Uknown"]! += workout.length
+                        }
                     }
                 }
-                minutesPerDayDict[Calendar.current.date(byAdding: .day, value: nextDay, to: firstMondayInWeek)!.formatted(.dateTime.weekday())] = minutesExercised / 60
+                graphs.append(BreakdownHistoryGraph(dateRange: nil, minutesPerDay: [
+                    minutesPerDay(dayOfWeek: .mon, time: (secondsPerDay["Mon"] ?? 0.0) / 60),
+                    minutesPerDay(dayOfWeek: .tue, time: (secondsPerDay["Tue"] ?? 0.0) / 60),
+                    minutesPerDay(dayOfWeek: .wed, time: (secondsPerDay["Wed"] ?? 0.0) / 60),
+                    minutesPerDay(dayOfWeek: .thu, time: (secondsPerDay["Thu"] ?? 0.0) / 60),
+                    minutesPerDay(dayOfWeek: .fri, time: (secondsPerDay["Fri"] ?? 0.0) / 60),
+                    minutesPerDay(dayOfWeek: .sat, time: (secondsPerDay["Sat"] ?? 0.0) / 60),
+                    minutesPerDay(dayOfWeek: .sun, time: (secondsPerDay["Sun"] ?? 0.0) / 60)
+                ], graphs: nil))
             }
-            return minutesPerDayDict
+            return graphs
+        }
+        
+        func getDateRanges(with data: FetchedResults<Workout>) -> Set<String> {
+            var ranges: Set<String> = []
+            for workout in data {
+                ranges.insert(workout.dateRange ?? "Uknown Range")
+            }
+            // need to sort ranges
+            return ranges
         }
         
     }
--- Fitness Tracker/Views/BreakdownHistory+View.swift
+++ Fitness Tracker/Views/BreakdownHistory+View.swift
@@ -5,92 +5,12 @@ struct BreakdownHistoryView: View {
     @FetchRequest(sortDescriptors: [SortDescriptor(\.dateCompleted)]) var workouts: FetchedResults<Workout>
     @Environment(\.managedObjectContext) var moc
     @ObservedObject var viewModel = ViewModel()
-
-    // go through each workout
-    // put date ranges in a set?
-    // if workout.dateRange is equal to each, assign minutes to respective days
-    func getDateRanges() -> Set<String> {
-        var ranges: Set<String> = []
-        for workout in workouts {
-            ranges.insert(workout.dateRange ?? "Uknown Range")
-        }
-        // need to sort ranges
-        return ranges
-    }
-    
-    func getExercisesForEachRange() -> [String:[Workout]] {
-        var exercises: [String:[Workout]] = [:]
-        for workout in workouts {
-            if exercises[workout.dateRange ?? "Uknown Range"] != nil {
-                exercises[workout.dateRange ?? "Uknown Range"]?.append(workout)
-            } else {
-                exercises[workout.dateRange ?? "Uknown Range"] = [workout]
-            }
-        }
-        // need to sort ranges
-        return exercises
-    }
-    
-    func getDateRange() -> [BreakdownHistoryGraph] {
-        var breakdownHistoryGraphs: [BreakdownHistoryGraph] = []
-        for range in getExercisesForEachRange().keys {
-            breakdownHistoryGraphs.append(BreakdownHistoryGraph(dateRange: range, minutesPerDay: nil, graphs: getTotalTimes()))
-        }
-        return breakdownHistoryGraphs
-    }
-    
-    func getTotalTimes() -> [BreakdownHistoryGraph] {
-        var graphs: [BreakdownHistoryGraph] = []
-        for range in getDateRanges() {
-            var secondsPerDay: [String:Double] = [
-                "Mon": 0.0,
-                "Tue": 0.0,
-                "Wed": 0.0,
-                "Thu": 0.0,
-                "Fri": 0.0,
-                "Sat": 0.0,
-                "Sun": 0.0
-            ]
-            for workout in workouts {
-                if workout.dateRange == range {
-                    switch workout.dateCompleted?.formatted(.dateTime.weekday()) {
-                    case "Mon":
-                        secondsPerDay["Mon"]! += workout.length
-                    case "Tue":
-                        secondsPerDay["Tue"]! += workout.length
-                    case "Wed":
-                        secondsPerDay["Wed"]! += workout.length
-                    case "Thu":
-                        secondsPerDay["Thu"]! += workout.length
-                    case "Fri":
-                        secondsPerDay["Fri"]! += workout.length
-                    case "Sat":
-                        secondsPerDay["Sat"]! += workout.length
-                    case "Sun":
-                        secondsPerDay["Sun"]! += workout.length
-                    default:
-                        secondsPerDay["Uknown"]! += workout.length
-                    }
-                }
-            }
-            graphs.append(BreakdownHistoryGraph(dateRange: nil, minutesPerDay: [
-                minutesPerDay(dayOfWeek: .mon, time: (secondsPerDay["Mon"] ?? 0.0) / 60),
-                minutesPerDay(dayOfWeek: .tue, time: (secondsPerDay["Tue"] ?? 0.0) / 60),
-                minutesPerDay(dayOfWeek: .wed, time: (secondsPerDay["Wed"] ?? 0.0) / 60),
-                minutesPerDay(dayOfWeek: .thu, time: (secondsPerDay["Thu"] ?? 0.0) / 60),
-                minutesPerDay(dayOfWeek: .fri, time: (secondsPerDay["Fri"] ?? 0.0) / 60),
-                minutesPerDay(dayOfWeek: .sat, time: (secondsPerDay["Sat"] ?? 0.0) / 60),
-                minutesPerDay(dayOfWeek: .sun, time: (secondsPerDay["Sun"] ?? 0.0) / 60)
-            ], graphs: nil))
-        }
-        return graphs
-    }
     
     var body: some View {
-        if getDateRange().isEmpty {
+        if viewModel.getDateRange(with: workouts).isEmpty {
             Text("Sorry, there is no data to display.")
         } else {
-            List(getDateRange(), children: \.graphs) { week in
+            List(viewModel.getDateRange(with: workouts), children: \.graphs) { week in
                     HStack {
                         if week.dateRange != nil {
                             Text(week.dateRange ?? "test")
