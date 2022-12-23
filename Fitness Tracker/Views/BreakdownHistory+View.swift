53c839540a8a097b1db3d0aa7f884ff8a7be85d9
Robert Hovey <robert.hovey95@gmail.com>
2022-12-23 08:41:04 -0700

    graph displaying correctly again

--- Fitness Tracker/Views/BreakdownHistory+View.swift
+++ Fitness Tracker/Views/BreakdownHistory+View.swift
@@ -11,43 +11,41 @@ struct BreakdownHistoryView: View {
             Text("Sorry, there is no data to display.")
         } else {
             List(viewModel.getDateRange(with: workouts), children: \.graphs) { week in
-                if week.dateRange != nil { // still not working...
-                        HStack {
-                            if week.dateRange != nil {
-                                Text(week.dateRange ?? "test")
-                            } else {
-                                Chart {
-                                    BarMark (
-                                        x: .value("Day", "Mon"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[0].time ?? 0.0)
-                                    )
-                                    BarMark (
-                                        x: .value("Day", "Tue"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[1].time ?? 0.0)
-                                    )
-                                    BarMark (
-                                        x: .value("Day", "Wed"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[2].time ?? 0.0)
-                                    )
-                                    BarMark (
-                                        x: .value("Day", "Thu"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[3].time ?? 0.0)
-                                    )
-                                    BarMark (
-                                        x: .value("Day", "Fri"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[4].time ?? 0.0)
-                                    )
-                                    BarMark (
-                                        x: .value("Day", "Sat"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[5].time ?? 0.0)
-                                    )
-                                    BarMark (
-                                        x: .value("Day", "Sun"),
-                                        y: .value("Total Minutes", week.minutesPerDay?[6].time ?? 0.0)
-                                    )
-                                }
-                                .frame(height: 250)
+                    HStack {
+                        if week.dateRange != nil {
+                            Text(week.dateRange ?? "test")
+                        } else {
+                            Chart {
+                                BarMark (
+                                    x: .value("Day", "Mon"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[0].time ?? 0.0)
+                                )
+                                BarMark (
+                                    x: .value("Day", "Tue"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[1].time ?? 0.0)
+                                )
+                                BarMark (
+                                    x: .value("Day", "Wed"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[2].time ?? 0.0)
+                                )
+                                BarMark (
+                                    x: .value("Day", "Thu"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[3].time ?? 0.0)
+                                )
+                                BarMark (
+                                    x: .value("Day", "Fri"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[4].time ?? 0.0)
+                                )
+                                BarMark (
+                                    x: .value("Day", "Sat"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[5].time ?? 0.0)
+                                )
+                                BarMark (
+                                    x: .value("Day", "Sun"),
+                                    y: .value("Total Minutes", week.minutesPerDay?[6].time ?? 0.0)
+                                )
                             }
+                            .frame(height: 250)
                         }
                     }
                     // should check if any time exists in the week
